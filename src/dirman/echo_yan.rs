683409f0204c125b7b71365f701579040430891b
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-14 13:52:37 +0800

    src: ownership's moves and transfers

--- src/dirman/echo_yan.rs
+++ src/dirman/echo_yan.rs
@@ -1,9 +1,12 @@
+use std::ffi::CString;
+
 /**
  * Author:  Nyxvectar Yan 
  * Repo:    rustHello
  * Created: 08/14/2025
  */
 
-pub fn echo_yan() {
-    println!("Nyxvectar Yan")
+pub fn echo_yan()-> String {
+    let s = String::from("Nyxvectar Yan");
+    s
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,18 @@ mod dirman {
 }
 
 fn main() {
-    {
-        echo_yan()
-    }
+    let s = String::from(echo_yan());
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
+    println!("{}", x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
