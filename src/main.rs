75d2610813d6ea31b836140f8ed86464a4a30a00
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-14 10:53:11 +0800

    basic value structures and others

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@
  */
 
 fn main() {
-    let (a, mut b): (bool,bool) = (true, false);
-    println!("a = {}, b = {}", a, b);
-    b = true;
-    assert_eq!(a, b);
-    panic!("手动panic")
+    let a = 255_u8;
+    let b = a.wrapping_add(1);
+    let c = 0.1_f32;
+    let mut d = 0.2_f32;
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("{}",abc.2);
+    println!("{}",b);
+    println!("{}",c+d);
 }
