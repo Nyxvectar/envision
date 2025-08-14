cf014cf6b607d2d5da8ca6079178ddb843390edf
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-14 13:44:44 +0800

    src: use mods from different directories

---
+++ src/dirman/echo_yan.rs
@@ -0,0 +1,9 @@
+/**
+ * Author:  Nyxvectar Yan 
+ * Repo:    rustHello
+ * Created: 08/14/2025
+ */
+
+pub fn echo_yan() {
+    println!("Nyxvectar Yan")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
+use crate::dirman::echo_yan::echo_yan;
+
 /**
  * Author:  Nyxvectar Yan 
  * Repo:    rustHello
  * Created: 08/13/2025
  */
 
-fn main() {
-    let a = 255_u8;
-    let b = a.wrapping_add(1);
-    let c = 0.1_f32;
-    let mut d = 0.2_f32;
-    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
-    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+mod dirman {
+    pub mod echo_yan;
+}
 
-    println!("{}",abc.2);
-    println!("{}",b);
-    println!("{}",c+d);
+fn main() {
+    {
+        echo_yan()
+    }
 }
