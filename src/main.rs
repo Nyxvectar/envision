af4fc9f9f97a4c9f1676ac5a4c56ac5183172bc6
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-15 10:42:36 +0800

    src: string, slice and tuple from some simples

--- src/main.rs
+++ src/main.rs
@@ -3,78 +3,47 @@ use crate::dirman::echo_yan::echo_yan;
 /**
  * Author:  Nyxvectar Yan
  * Repo:    rust-learn
- * Created: 08/13/2025
+ * Created: 08/15/2025
  */
 
 mod dirman {
     pub mod echo_yan;
 }
 
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
-}
-
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
-
-enum Message {
-    Quit,
-    Move { x:i32, y:i32 },
-    Write(String),
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
 fn main() {
-    let score = 85;
-    let msg = Message::Move { x:30, y:40 };
+    let email = "Nyxvectar@proton.me";
+    let mut user1 = User {
+        email: String::from(email),
+        username: String::from(""),
+        active: true,
+        sign_in_count: 1,
+    };
+
     
-    print_name();
-    handle_errs();
-    
-    {
-        match score {
-            90..=100 => println!("优秀"), // 90 到 100（包含）
-            80..=89 => println!("良好"),  // 80 到 89
-            60..=79 => println!("及格"),
-            0..=59 => println!("不及格"),
-            _ => println!("分数无效"), // 处理负数或超过 100 的情况
-        }
-    }
-    
-    match msg { 
-        Message::Quit => {
-            println!("Quit")
-        }
-        Message::Move { x, y} => {
-            println!("Move to {}, {}", x,y)
-        }
-        Message::Write(text) => {
-            println!("Write {}", text)
-        }
-    }
-    println!("Coin {}", value_in_cents(Coin::Quarter))
+    user1.username = echo_yan();
+    println!("{}", user1.username);
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    println!("{}", user1.active);
+    // println!("{:?}", user1);
 }
 
-fn handle_errs() {
-    //TODO.
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin { 
-        Coin::Dime => 1,
-        Coin::Nickel => 2,
-        Coin::Penny => 3,
-        Coin::Quarter => 4,
+fn build_user(email: String, username:String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
     }
-}
-
-fn print_name() {
-    let mut name = "Nyxvectar";
-    name = "Nyxvectar Yan";
-    println!("{}",echo_yan());
-    println!("{}", name);
 }
