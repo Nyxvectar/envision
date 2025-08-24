7db11eb8a877c000986a514317256860b676794a
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-24 08:42:01 +0800

    p3954

--- src/p/5/p5708.rs
+++ src/p/5/p5708.rs
@@ -1,14 +1,20 @@
+/**
+ * Author:  Nyxvectar Yan
+ * Repo:    rustHello
+ * Created: 08/24/2025
+ */
 use std::io;
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let get: Vec<i32> = input
+    let get: Vec<f64> = input
         .trim()
         .split_whitespace()
         .map(|x| x.parse().unwrap())
         .collect();
     let (a, b, c) = (get[0], get[1], get[2]);
-    let grade = (a * 2 + b * 3 + c * 5) / 10;
-    println!("{}", grade)
+    let p = (a + b + c) / 2.0;
+    let s = (p * (p - a) * (p - b) * (p - c)).sqrt();
+    print!("{:.1}", s);
 }
