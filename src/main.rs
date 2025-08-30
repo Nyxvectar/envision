9b299211305e7068c92a5720a626819d90d039ce
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-30 15:54:39 +0800

    p5716-re

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,6 @@ use std::io;
 
 fn main() {
     let input = get_input(2);
-    let mut result = 0;
-    if input[0] % 100 == 0 {
-        if input[0] % 400 == 0 {
-            result = match_month(true, input[1]);
-        } else {
-            result = match_month(false, input[1]);
-        }
-    } else {
-        if input[0] % 4 == 0 {
-            result = match_month(true, input[1]);
-        } else {
-            result = match_month(false, input[1]);
-        }
-    }
-    print!("{}", result)
-}
-
-fn match_month(year: bool, month: i64) -> i64 {
-    match month {
-        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
-        4 | 6 | 9 | 11 => 30,
-        2 => {
-            if year == true {
-                29
-            } else {
-                28
-            }
-        }
-        _ => 0,
-    }
 }
 
 fn get_input(how_many_need: i64) -> Vec<i64> {
