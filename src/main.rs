0bd197d187a990b129ecf66e97238fec452effb5
Nyxvectar Yan <Nyxvectar@proton.me>
2025-08-30 18:14:51 +0800

    readme.md

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use std::arch::x86_64::_xgetbv;
-use std::cmp::max;
 use std::io;
 
 fn main() {
     let mut values = get_input(3);
-    values.sort();
-    let (a, b, c) = (values[0], values[1], values[2]);
-    let orders = get_string_input();
-    for k in 0..3 {
-        match orders.chars().nth(k).unwrap() {
-            'A' => {
-                print!("{} ", a)
-            }
-            'B' => {
-                print!("{} ", b)
-            }
-            _ => {
-                print!("{} ", c)
-            }
-        }
-    }
 }
 
 fn get_input(how_many_need: i64) -> Vec<i64> {
@@ -31,10 +13,4 @@ fn get_input(how_many_need: i64) -> Vec<i64> {
         .map(|x| x.parse().unwrap())
         .collect();
     get.into_iter().take(how_many_need as usize).collect()
-}
-
-fn get_string_input() -> String {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    input.trim().to_string()
 }
