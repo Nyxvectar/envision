2e3aa6ffa80df18c16c18102c3ba186dcd9753cb
Nyxvectar Yan <Nyxvectar@proton.me>
2025-09-06 20:28:34 +0800

    p1720

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+use num::integer::sqrt;
+use num::{Float, pow};
 use std::io;
 
 fn main() {
-    let get = input(1)[0];
+    let get = input(1);
 }
 
-fn input(how_many_need: i64) -> Vec<f64> {
+fn input(how_many_need: i64) -> Vec<i64> {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let get: Vec<f64> = input
+    let get: Vec<i64> = input
         .trim()
         .split_whitespace()
         .map(|x| x.parse().unwrap())
---
+++ src/p/1/p1720.rs
@@ -0,0 +1,18 @@
+/**
+ * Author:  Nyxvectar Yan 
+ * Repo:    rustHello
+ * Created: 09/06/2025
+ */
+
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: u32 = input.trim().parse().unwrap();
+    let sqrt5 = 5.0_f64.sqrt();
+    let a = (1.0 + sqrt5) / 2.0;
+    let b = (1.0 - sqrt5) / 2.0;
+    let fn_value = (a.powi(n as i32) - b.powi(n as i32)) / sqrt5;
+    println!("{:.2}", fn_value);
+}
