27bf2b68968e08477a4491a08bd4dc1b76a540c2
Nyxvectar Yan <Nyxvectar@proton.me>
2025-09-07 14:30:14 +0800

    q58

---
+++ src/l/5/q58.rs
@@ -0,0 +1,22 @@
+/**
+ * Author:  Nyxvectar Yan
+ * Repo:    rustHello
+ * Created: 09/07/2025
+ */
+
+struct Solution;
+
+fn main() {
+    let result = 1;
+    println!("{:?}", result);
+}
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let words: Vec<&str> = s.trim().split_whitespace().collect();
+        match words.last() {
+            Some(word) => word.len() as i32,
+            None => 0,
+        }
+    }
+}
