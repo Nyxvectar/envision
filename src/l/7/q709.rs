1c3243ca580e218ad3267a43e9bf7ad748f9f5ce
Nyxvectar Yan <Nyxvectar@proton.me>
2025-09-07 10:05:42 +0800

    q709

---
+++ src/l/7/q709.rs
@@ -0,0 +1,11 @@
+/**
+ * Author:  Nyxvectar Yan
+ * Repo:    rustHello
+ * Created: 09/07/2025
+ */
+
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        s.to_ascii_lowercase()
+    }
+}
