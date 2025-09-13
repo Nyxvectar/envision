303e42f75e23b206ed449d3548b7468b5fb1739b
Raye Lattice <Nyxvectar@proton.me>
2025-09-13 22:54:16 +0800

    q283

--- src/leetcode/2/q283.rs
+++ src/leetcode/2/q283.rs
@@ -1,7 +1,13 @@
+/**
+ * Author:  Raye Lattice
+ * Repo:    rustHello
+ * Created: 09/13/2025
+ */
+
 struct Solution;
 
 fn main() {
-    let result = "abcabcabcabc";
+    let result = "Raye Lattice";
     println!("{:?}", result);
 }
 
