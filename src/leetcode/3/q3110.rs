0f6587e64093b370dd97d272e765009c58c7112c
Raye Lattice <nyxvectar@proton.me>
2025-09-14 10:52:24 +0800

    q3280

--- src/leetcode/3/q3110.rs
+++ src/leetcode/3/q3110.rs
@@ -8,7 +8,7 @@ impl Solution {
     pub fn score_of_string(s: String) -> i32 {
         s.as_bytes()
             .windows(2)
-            .map(|x| {(x[1]as i32 - x[0] as i32).abs()})
+            .map(|x| (x[1] as i32 - x[0] as i32).abs())
             .sum()
     }
 }
--- src/leetcode/3/q3280.rs
+++ src/leetcode/3/q3280.rs
@@ -1,9 +1,8 @@
-struct Solution;
-
-fn main() {
-    let result = "Raye Lattice";
-    println!("{:?}", result);
-}
+/**
+ * Author:  Raye Lattice
+ * Repo:    rustHello
+ * Created: 09/14/2025
+ */
 
 impl Solution {
     pub fn convert_date_to_binary(date: String) -> String {
